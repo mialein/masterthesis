b621ab6c96c1f025f3bec92a3e143f618244c659
Maria Seidens <maria.seidens@stud.tu-darmstadt.de>
2018-08-05 16:29:13 +0200

    show captcha image

--- wallstreet/wallstreet/pipelines.py
+++ wallstreet/wallstreet/pipelines.py
@@ -5,7 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import logging
 
-class WallstreetPipeline(object):
-    def process_item(self, item, spider):
-        return item
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy import Request
+from io import BytesIO
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+
+
+class WallstreetPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        logging.debug('get media requests: {}'.format(item))
+        filename = '{}.jpg'.format(item['image_name'][0])
+        yield Request(item['image_urls'][0], meta={'filename': filename})
+
+    def image_downloaded(self, response, request, info):
+        logging.debug('response.body: {}'.format(response.body))
+        image = Image.open(BytesIO(response.body))
+        image.show()
