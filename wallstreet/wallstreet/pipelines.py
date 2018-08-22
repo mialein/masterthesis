23059eb55f9ff8c6bf79fdf7a886ade203b4bf46
Maria Brockmeyer <maria.seidens@stud.tu-darmstadt.de>
2018-08-23 00:49:36 +0200

    write items to mongodb

--- wallstreet/wallstreet/pipelines.py
+++ wallstreet/wallstreet/pipelines.py
@@ -5,3 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+from scrapy.conf import settings
+
+
+class MongoPipeline(object):
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        self.db = self.client[settings['MONGODB_DB']]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[settings['MONGODB_COLLECTION']].insert_one(dict(item))
+        return item
