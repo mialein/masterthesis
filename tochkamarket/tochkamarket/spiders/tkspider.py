ff9bec25ca4a9d14f411842031d2432ab0161604
Maria Brockmeyer <maria.seidens@stud.tu-darmstadt.de>
2018-10-08 22:51:36 +0200

    solve captcha

--- tochkamarket/tochkamarket/spiders/tkspider.py
+++ tochkamarket/tochkamarket/spiders/tkspider.py
@@ -1,8 +1,12 @@
 from scrapy.spiders import Spider
-import json
+from scrapy import FormRequest
+from scrapy.selector import Selector
 import logging
+import json
 
-from io import open
+from io import BytesIO, open
+import tkinter
+from PIL import ImageTk, Image
 
 class MySpider(Spider):
     name = "tkspider"
@@ -12,11 +16,37 @@ class MySpider(Spider):
         pass
 
     def login(self, response):
-        logging.debug("in login")
-        pass
+        logging.debug("in login()")
+        image = Image.open(BytesIO(response.body))
+        top = tkinter.Tk()
+        top.title("Solving Captcha")
+        top.geometry("400x200")
+        img = ImageTk.PhotoImage(image, size="400x200")
+        imagelabel = tkinter.Label(top, image=img)
+        textentry = tkinter.Entry(top, font = "Helvetica 20 bold")
+        textentry.focus_set()
+
+        original_response = response.meta['original response']
+
+        formdata = {
+            'username': json.load(open('config'))['username'],
+            'passphrase': json.load(open('config'))['password']
+        }
+
+        def callback(en):
+            formdata['captcha'] = textentry.get()
+            top.destroy()
+
+        textentry.bind("<Return>", callback)
+        imagelabel.pack(side = "top", fill = "both", expand = "yes")
+        textentry.pack(side = "bottom", fill = "both", expand = "yes")
+        top.mainloop()
+
+        logging.debug('applying formdata in original response {}'.format(original_response))
+        yield FormRequest.from_response(original_response, formdata=formdata, callback=self.click_all_drugs)
 
     def click_all_drugs(self, response):
-        pass
+        logging.debug('in click_all_drugs()')
 
     def click_all_pages(self, response):
         pass
