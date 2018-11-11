cba976cf0b726f0647244b51564f2ed4595270c7
Maria Brockmeyer <maria.seidens@stud.tu-darmstadt.de>
2018-11-11 17:56:15 +0100

    add filter for shipping

--- wallstreet/wallstreet/analyzer.py
+++ wallstreet/wallstreet/analyzer.py
@@ -22,13 +22,15 @@ if __name__ == '__main__':
 
     currency = sys.argv[2] if len(sys.argv) > 2 else '€' 
     price_unit = sys.argv[3] if len(sys.argv) > 3 else 'gram'
+    ships_from = sys.argv[4] if len(sys.argv) > 4 else None
+    ships_to = sys.argv[5] if len(sys.argv) > 5 else None
 
     docs = [d for d in docs]
     for doc in docs:
         doc['price_unit'] = doc['price_unit'].lower().strip().replace('/', '')
         doc['ships_from'] = ' '.join(s.replace('Ships from:', '').strip() for s in doc['ships_from'].split('\n')).strip()
-        ships_to = ' '.join(s.replace('Only ships to certain countries', '').strip() for s in doc['ships_to'].split('\n')).strip()
-        doc['ships_to'] = ['Worldwide'] if 'Worldwide' in ships_to else [s.strip() for s in ships_to.split(',')]
+        stripped = ' '.join(s.replace('Only ships to certain countries', '').strip() for s in doc['ships_to'].split('\n')).strip()
+        doc['ships_to'] = ['Worldwide'] if 'Worldwide' in stripped else [s.strip() for s in stripped.split(',')]
 
     print('{} docs'.format(len(docs)))
 
@@ -37,8 +39,11 @@ if __name__ == '__main__':
     print(Counter(d['ships_from'] for d in docs))
     print(Counter(t for d in docs for t in d['ships_to']))
 
-    eur_grams = {(doc['title'], doc['date']): doc for doc in docs if currency in doc['price'] and doc['price_unit'] == price_unit}.values()
-    print('{} docs with {}/{}'.format(len(eur_grams), currency, price_unit))
+    filtered_docs = {(doc['title'], doc['date']): doc for doc in docs # filter duplicate dates, TODO: rather differentiate by timestep, too
+            if currency in doc['price'] and doc['price_unit'] == price_unit and
+                (ships_from is None or ships_from == doc['ships_from']) and (ships_to is None or ships_to in doc['ships_to'])
+            }.values()
+    print('{} docs with {}/{} shipping from {} to {}'.format(len(filtered_docs), currency, price_unit, ships_from, ships_to))
 
     def to_float(price):
         try:
@@ -51,25 +56,26 @@ if __name__ == '__main__':
             print('failed: ' + price)
             return None
 
-    eur_grams = [{
+    filtered_docs = [{
         'title': d['title'],
         'date': dt.datetime.strptime(d['date'],'%d.%m.%Y').date(),
         'price': to_float(d['price'].strip(currency))}
-        for d in eur_grams]
+        for d in filtered_docs]
 
-    find_multi = re.compile(r'(\d+)\s*(gr|g)', re.I) #case insensitive
+    if price_unit == 'gram':
+        find_multi = re.compile(r'(\d+)\s*(gr|g)', re.I) #case insensitive
 
-    for eg in eur_grams:
-        match = find_multi.search(eg['title'])
-        if match:
-            div = int(match.group(1))
-            if div != 0:
-                eg['price'] /= div
+        for eg in filtered_docs:
+            match = find_multi.search(eg['title'])
+            if match:
+                div = int(match.group(1))
+                if div != 0:
+                    eg['price'] /= div
 
 
-    dates = sorted({d['date'] for d in eur_grams})
+    dates = sorted({d['date'] for d in filtered_docs})
 
-    prices_per_date = {date: [p['price'] for p in eur_grams if p['date'] == date] for date in dates} # group by dates
+    prices_per_date = {date: [p['price'] for p in filtered_docs if p['date'] == date] for date in dates} # group by dates
 
     labels = ['offer count', 'median price']
 
@@ -85,6 +91,6 @@ if __name__ == '__main__':
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
         plt.plot(dates, y[label])
 
-        plt.legend([label], loc='upper right')
+        plt.legend([label], loc='lower right')
         plt.xticks(dates[::1], rotation=90)
         plt.show()
