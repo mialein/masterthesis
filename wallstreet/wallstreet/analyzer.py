bb7d81854e49b23bf232de8de7dc51aa4a61e0e4
Maria Brockmeyer <maria.seidens@stud.tu-darmstadt.de>
2018-11-11 02:56:07 +0100

    generalize analyzer

--- wallstreet/wallstreet/analyzer.py
+++ wallstreet/wallstreet/analyzer.py
@@ -1,73 +1,70 @@
 if __name__ == '__main__':
     import pymongo
     from scrapy.conf import settings
-    import time
     import numpy as np
+    import datetime as dt
+    import sys
+    from collections import Counter
 
     with pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT']) as client:
         db = client[settings['MONGODB_DB']]
         table = db[settings['MONGODB_COLLECTION']]
 
-        query = {'drug_type': 'weed'}
-
+        drug = sys.argv[1]
+        query = {'drug_type': drug}
         docs = table.find(query)
 
-        stripped_docs = [{'title': d['title'], 'date': d['date'], 'price': d['price'], 'price_unit': d['price_unit'].strip()} for d in docs]
-        print('{} stripped docs'.format(len(stripped_docs)))
+    stripped_docs = [{'title': d['title'], 'date': d['date'], 'price': d['price'], 'price_unit': d['price_unit'].strip()} for d in docs]
+    print('{} stripped docs'.format(len(stripped_docs)))
 
-        print({d['price_unit'] for d in stripped_docs}) #{'/Milligram', '/Ounce', '/Gram', '/Piece'}
+    print(Counter(d['price_unit'] for d in stripped_docs)) #{'/Milligram', '/Ounce', '/Gram', '/Piece'}
 
-        eur_grams = [d for d in stripped_docs if '€' in d['price'] and d['price_unit'] == '/Gram']
-        print('{} docs with EUR/Gram'.format(len(eur_grams)))
+    eur_grams = {(doc['title'], doc['date']): doc for doc in stripped_docs if '€' in doc['price'] and doc['price_unit'] == '/Gram'}.values()
+    print('{} docs with EUR/Gram'.format(len(eur_grams)))
 
-        def to_float(str):
-            try:
-                if ',' in str:
-                    return float(str.replace('.', '').replace(',', '.'))
-                else:
-                    return float(str)
-            except:
-                print('failed: ' + str)
-                return None
+    def to_float(str):
+        try:
+            if ',' in str:
+                return float(str.replace('.', '').replace(',', '.'))
+            else:
+                return float(str)
+        except:
+            print('failed: ' + str)
+            return None
 
-        eur_grams = [{'title': d['title'], 'date': time.strptime(d['date'], '%d.%m.%Y'), 'price': to_float(d['price'].strip('€'))} for d in eur_grams]
+    eur_grams = [{
+        'title': d['title'],
+        'date': dt.datetime.strptime(d['date'],'%d.%m.%Y').date(),
+        'price': to_float(d['price'].strip('€'))}
+        for d in eur_grams]
 
-        import re
-        find_multi = re.compile(r'(\d+)\s*(gr|g)', re.I) #case insensitive
+    import re
+    find_multi = re.compile(r'(\d+)\s*(gr|g)', re.I) #case insensitive
 
-        for eg in eur_grams:
-            match = find_multi.search(eg['title'])
-            if match:
-                eg['price'] /= int(match.group(1))
+    for eg in eur_grams:
+        match = find_multi.search(eg['title'])
+        if match:
+            eg['price'] /= int(match.group(1))
 
 
-        dates = sorted({d['date'] for d in eur_grams})
+    dates = sorted({d['date'] for d in eur_grams})
 
-        prices_per_date = {d: np.asarray([p['price'] for p in eur_grams if p['date'] == d]) for d in dates} # group by dates
+    prices_per_date = {date: [p['price'] for p in eur_grams if p['date'] == date] for date in dates} # group by dates
 
-        data_per_date = {date: {'offer count': len(prices),
-                                'max price': np.max(prices),
-                                'min price': np.min(prices),
-                                'mean price': np.mean(prices),
-                                'median price': np.median(prices),
-                                'std dev': np.std(prices)
-                                }
-                         for date, prices in prices_per_date.items()}
+    labels = ['offer count', 'median price']
 
-        prices_per_date_2sigma = {d: np.asarray([p for p in prices_per_date[d] if p < data_per_date[d]['mean price'] + 2*data_per_date[d]['std dev']]) for d in prices_per_date}
+    data_per_date = {date: {labels[0]: len(prices), labels[1]: np.median(prices)}
+                     for date, prices in prices_per_date.items()}
 
-        data_per_date_2sigma = {date: {'offer count': len(prices),
-                                'max price': np.max(prices),
-                                'min price': np.min(prices),
-                                'mean price': np.mean(prices),
-                                'median price': np.median(prices),
-                                'std dev': np.std(prices)
-                                }
-                         for date, prices in prices_per_date_2sigma.items()}
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
 
-        for date, data in sorted(data_per_date.items()):
-            print(time.strftime('%d.%m.%Y', date), data)
+    y = {label: [data[label] for date, data in sorted(data_per_date.items())] for label in labels if label}
 
-        for date, data in sorted(data_per_date_2sigma.items()):
-            print(time.strftime('%d.%m.%Y', date), data)
+    for label in y:
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
+        plt.plot(dates, y[label])
 
+        plt.legend([label], loc='upper right')
+        plt.xticks(dates[::2], rotation=90)
+        plt.show()
