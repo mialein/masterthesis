3be4ec52a0b24cfbf3eb83d4346f37f9c5c647ee
Maria Brockmeyer <maria.seidens@stud.tu-darmstadt.de>
2018-11-11 15:42:21 +0100

    extract shipping info

--- wallstreet/wallstreet/analyzer.py
+++ wallstreet/wallstreet/analyzer.py
@@ -7,31 +7,38 @@ if __name__ == '__main__':
     import re
     from collections import Counter
 
+    drug_list = ['weed', 'hashish', 'concentrates', 'cocaine', 'meth', 'speed',
+            'lsd', 'mdma', 'benzos', 'ecstasy', 'opiates', 'steroids']
+
     with pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT']) as client:
         db = client[settings['MONGODB_DB']]
         table = db[settings['MONGODB_COLLECTION']]
 
         drug = sys.argv[1]
+        if drug not in drug_list:
+            raise Exception('invalid drug: ' + drug)
         query = {'drug_type': drug}
         docs = table.find(query)
 
     currency = sys.argv[2] if len(sys.argv) > 2 else '€' 
+    price_unit = sys.argv[3] if len(sys.argv) > 3 else 'gram'
 
     docs = [d for d in docs]
     for doc in docs:
-        doc['price_unit'] = doc['price_unit'].strip()
+        doc['price_unit'] = doc['price_unit'].lower().strip().replace('/', '')
         doc['ships_from'] = ' '.join(s.replace('Ships from:', '').strip() for s in doc['ships_from'].split('\n')).strip()
-        doc['ships_to'] = ' '.join(s.replace('Only ships to certain countries', '').strip() for s in doc['ships_to'].split('\n')).strip()
+        ships_to = ' '.join(s.replace('Only ships to certain countries', '').strip() for s in doc['ships_to'].split('\n')).strip()
+        doc['ships_to'] = ['Worldwide'] if 'Worldwide' in ships_to else [s.strip() for s in ships_to.split(',')]
 
     print('{} docs'.format(len(docs)))
 
     print(Counter(d['price_unit'] for d in docs))
     print(Counter(re.sub(r'[^\$\€]', '', d['price']) for d in docs))
     print(Counter(d['ships_from'] for d in docs))
-    print(Counter(d['ships_to'] for d in docs))
+    print(Counter(t for d in docs for t in d['ships_to']))
 
-    eur_grams = {(doc['title'], doc['date']): doc for doc in docs if currency in doc['price'] and doc['price_unit'] == '/Gram'}.values()
-    print('{} docs with {}/Gram'.format(len(eur_grams), currency))
+    eur_grams = {(doc['title'], doc['date']): doc for doc in docs if currency in doc['price'] and doc['price_unit'] == price_unit}.values()
+    print('{} docs with {}/{}'.format(len(eur_grams), currency, price_unit))
 
     def to_float(price):
         try:
